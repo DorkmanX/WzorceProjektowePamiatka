d009ebdb4821886095d14c2074bdfd7b6fab562d
Kamil Rzeźniczek <kamil.rzezniczek@gmail.com>
2023-04-15 15:08:01 +0200

    Added revert move to design pattern

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ public class Program
         }
         public void MakeMove()
         {
+            BEGIN:
             string message;
             PrintBoard();
 
@@ -23,8 +24,18 @@ public class Program
                 message = "Teraz kolej ma: X";
             else
                 message = "Teraz kolej ma: O";
-            Console.WriteLine("Następna kolej: " + message);
 
+            Console.WriteLine("Czy chcesz cofnąć ruch ? Wpisz 1 jeżeli tak, 0 jeżeli nie");
+            int revertMove = Convert.ToInt32(Console.ReadLine());
+
+            if (revertMove == 1)
+            {
+                ReverseMove();
+                PrintBoard();
+                goto BEGIN;
+            }
+
+            Console.WriteLine("Następna kolej: " + message);
             Console.Write("Wpisz wiersz pola do strzalu:");
             int strzal = Convert.ToInt32(Console.ReadLine());
             Console.Write("Wpisz kolumne pola do strzalu:");
@@ -33,11 +44,12 @@ public class Program
             state.SetBoardState(strzal, strzal2);
             state.SetNextMove();
 
-            stack.Add(state);
+            stack.Add(new Memento(state));
         }
         public void ReverseMove()
         {
-            Console.WriteLine("Czy chcesz cofnąć ruch ? Wpisz 1 jeżeli tak, 0 jeżeli nie");
+            stack.Delete(state);
+            state = (State)stack.GetLast();
         }
         public void PrintBoard()
         {
@@ -61,7 +73,7 @@ public class Program
 
             public Memento(State State)
             {
-                _State = State;
+                _State = new State(State);
             }
         }
         internal class State
@@ -70,21 +82,39 @@ public class Program
             // 1 -> X   -1 -> O //zaczyna X
             private List<List<int>> _Board;
             // 1 -> X 0 -> Puste pole  -1 -> O
-            public State() 
+            private int maxColumnNumber;
+            private int maxRowNumber;
+            public State()
             {
                 _NextMove = 1;
                 _Board = new List<List<int>>();
-                for(int i = 0; i < 3; i++) 
+                for (int i = 0; i < 3; i++)
                 {
-                    _Board.Add(new List<int>() { 0,0,0}); //empty field
+                    _Board.Add(new List<int>() { 0, 0, 0 }); //empty field
                 }
+                maxColumnNumber = 2;
+                maxRowNumber = 2;
+            }
+            public State(State state)
+            {
+                _NextMove = state._NextMove; 
+                _Board = state._Board;
+                maxColumnNumber = 2;
+                maxRowNumber = 2;
             }
             public void SetBoardState(int index, int index2)
             {
                 if (_NextMove == 1)
-                    _Board[index][index2] = 1;
+                {
+                    if (index <= maxColumnNumber && index2 <= maxRowNumber && (index >= 0 && index2 >= 0))
+                        _Board[index][index2] = 1;
+                    
+                }
                 else
-                    _Board[index][index2] = -1;    
+                {
+                    if (index <= maxColumnNumber && index2 <= maxRowNumber && (index >= 0 && index2 >= 0))
+                        _Board[index][index2] = -1;
+                }
             }
             public List<List<int>> GetBoardState()
             {
