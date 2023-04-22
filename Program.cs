3588f4aac5db57b19789ea32d6c07ea58b2bee4f
Kamil Rzeźniczek <kamil.rzezniczek@gmail.com>
2023-04-22 11:37:13 +0200

    Fixed memory cloning

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Reflection;
+using System.Text.Json;
+using System.Xml.Linq;
 using static Program;
 
 
@@ -11,7 +14,15 @@ public class Program
         public Game() 
         {
             stack = new Caretaker();
-            state = new State();
+
+            List<List<int>> board = new List<List<int>>();
+            for (int i = 0; i < 3; i++)
+            {
+                board.Add(new List<int>() { 0, 0, 0 });
+            }
+
+            state = new State(1,board,2,2);
+            stack.Add(new Memento(State.DeepCopy(state)));
         }
         public void MakeMove()
         {
@@ -31,7 +42,6 @@ public class Program
             if (revertMove == 1)
             {
                 ReverseMove();
-                PrintBoard();
                 goto BEGIN;
             }
 
@@ -41,15 +51,20 @@ public class Program
             Console.Write("Wpisz kolumne pola do strzalu:");
             int strzal2 = Convert.ToInt32(Console.ReadLine());
 
-            state.SetBoardState(strzal, strzal2);
-            state.SetNextMove();
+            State current_state = State.DeepCopy(state);
+            current_state.SetBoardState(strzal, strzal2);
+            current_state.SetNextMove();
 
-            stack.Add(new Memento(state));
+            Memento current_state_memento = new Memento(current_state);
+            stack.Add(current_state_memento);
+
+            state = current_state;
         }
         public void ReverseMove()
         {
-            stack.Delete(state);
-            state = (State)stack.GetLast();
+            stack.DeleteLast();
+            Memento old_memento = (Memento)stack.GetLast();
+            state = State.DeepCopy(old_memento._State);
         }
         public void PrintBoard()
         {
@@ -69,62 +84,56 @@ public class Program
         }
         internal class Memento
         {
-            private State _State;
-
-            public Memento(State State)
+            public State _State { get; private set; }
+            public Memento(State state)
             {
-                _State = new State(State);
+                _State = state;
             }
+            public Memento() { }
         }
         internal class State
         {
-            private int _NextMove;
-            // 1 -> X   -1 -> O //zaczyna X
-            private List<List<int>> _Board;
-            // 1 -> X 0 -> Puste pole  -1 -> O
-            private int maxColumnNumber;
-            private int maxRowNumber;
-            public State()
+            public int _NextMove { get; set; }
+            public List<List<int>> _Board{ get; set; }
+            public int maxColumnNumber { get; set; }
+            public int maxRowNumber { get; set; }
+            public State() { }
+            public State(int nextMove, List<List<int>> board, int maxColumnNumber, int maxRowNumber)
             {
-                _NextMove = 1;
-                _Board = new List<List<int>>();
-                for (int i = 0; i < 3; i++)
-                {
-                    _Board.Add(new List<int>() { 0, 0, 0 }); //empty field
-                }
-                maxColumnNumber = 2;
-                maxRowNumber = 2;
+                _NextMove = nextMove;
+                _Board = board;
+                this.maxColumnNumber = maxColumnNumber;
+                this.maxRowNumber = maxRowNumber;
             }
-            public State(State state)
-            {
-                _NextMove = state._NextMove; 
-                _Board = state._Board;
-                maxColumnNumber = 2;
-                maxRowNumber = 2;
-            }
+
             public void SetBoardState(int index, int index2)
             {
-                if (_NextMove == 1)
+                if (this._NextMove == 1)
                 {
                     if (index <= maxColumnNumber && index2 <= maxRowNumber && (index >= 0 && index2 >= 0))
-                        _Board[index][index2] = 1;
+                        this._Board[index][index2] = 1;
                     
                 }
                 else
                 {
                     if (index <= maxColumnNumber && index2 <= maxRowNumber && (index >= 0 && index2 >= 0))
-                        _Board[index][index2] = -1;
+                        this._Board[index][index2] = -1;
                 }
             }
             public List<List<int>> GetBoardState()
             {
-                return _Board;
+                return this._Board;
             }
             public void SetNextMove() 
             {
-                _NextMove *= -1;
+                this._NextMove *= -1;
             }
-            public int GetNextMove() { return _NextMove; }
+            public int GetNextMove() { return this._NextMove; }
+            public static T DeepCopy<T>(T input)
+            {
+                var jsonString = JsonSerializer.Serialize(input);
+                return JsonSerializer.Deserialize<T>(jsonString);
+            }
         }
     }
     public class Caretaker
@@ -138,9 +147,9 @@ public class Program
         {
             statesList.Add(memento);
         }
-        public void Delete(object memento) 
+        public void DeleteLast() 
         {
-            statesList.Remove(memento);
+            statesList.RemoveAt(statesList.Count - 1);
         }
         public object GetLast() 
         {
